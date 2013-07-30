ebc31faf49ee7f3abc4efd2bdce98eb438d8ccd4
fusionBIn <iahu1988 (at) gmail.com>
2013-07-30 09:29:32 +0800

    create files
    create files

---
+++ README.md
@@ -0,0 +1,6 @@
+# escape
+
+ 一个类似于 JavaScript escape() 的 sublime 插件。可以把中文等非 ASCII 码转义成
+ unicode_escape 码。防止出现乱码问题。
+
+ JavaScript escape() like tool for sublime. escape js strings to unicode_escape.
---
+++ escape.py
@@ -0,0 +1,9 @@
+# encoding=utf-8
+import sublime, sublime_plugin, json
+
+class EscapeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sels = self.view.sel();
+        for sel in sels:
+            t = self.view.substr(sel);
+            self.view.replace(edit, sel, json.dumps(t).strip('"'));
