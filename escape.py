d9fd72e3d7bad1ddb9dfcb49406ca29802031f6f
iahu <iahu1988@gmail.com>
2015-09-09 13:36:28 +0800

    add unescape function

--- escape.py
+++ escape.py
@@ -2,8 +2,16 @@
 import sublime, sublime_plugin, json
 
 class EscapeCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        sels = self.view.sel();
-        for sel in sels:
-            t = self.view.substr(sel);
-            self.view.replace(edit, sel, json.dumps(t).strip('"'));
+	def run(self, edit):
+		sels = self.view.sel();
+		for sel in sels:
+			t = self.view.substr(sel);
+			self.view.replace(edit, sel, json.dumps(t).strip('"'));
+
+class UnescapeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sels = self.view.sel();
+		for sel in sels:
+			t = self.view.substr(sel);
+			self.view.replace(edit, sel, json.loads( '["'+t+'"]' )[0] );
+			# 中文
